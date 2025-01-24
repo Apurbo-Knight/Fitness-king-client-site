00e03b0f3506c1b7fb56dcc79efeecf7dcc22f81
Apurbo Das <akdjoy29@gmail.com>
2025-01-25 05:30:36 +0600

    update our team section

--- src/Pages/Home/Team.jsx
+++ src/Pages/Home/Team.jsx
@@ -45,11 +45,11 @@ const Team = () => {
               <div className="parent shadow-md  relative group-hover:transform group-hover:scale-105 duration-500 ">
                 <img src={t1} alt="" className="w-full rounded-t-md h-80 object-cover" />
               </div>
-              <div className="child bg-black bg-opacity-70 text-white  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
+              <div className="child bg-black bg-opacity-70 text-teal-400  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
                 <h1 className="text-xl font-semibold"> 
                 Emily Smith
                 </h1>
-                <p className="text-sm mt-1 text-gray-500 group-hover:text-white hidden group-hover:block duration-500 ">
+                <p className="text-sm mt-1  text-teal-400 group-hover:text-tral-400 hidden group-hover:block duration-500 ">
                 Certified Personal Trainer
                 </p>
               </div>
@@ -60,11 +60,11 @@ const Team = () => {
               <div className="parent shadow-md  relative group-hover:transform group-hover:scale-105 duration-500 ">
                 <img src={t4} alt="" className="w-full rounded-t-md h-80 object-cover" />
               </div>
-              <div className="child bg-black bg-opacity-70 text-white  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
+              <div className="child bg-black bg-opacity-70 text-teal-400  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
                 <h1 className="text-xl font-semibold"> 
                 Michael Johnson
                 </h1>
-                <p className="text-sm mt-1 text-gray-500 group-hover:text-white hidden group-hover:block duration-500 ">
+                <p className="text-sm mt-1 text-teal-400 group-hover:text-teal-400 hidden group-hover:block duration-500 ">
                 Cardiovascular Fitness Expert
                 </p>
               </div>
@@ -75,11 +75,11 @@ const Team = () => {
               <div className="parent shadow-md  relative group-hover:transform group-hover:scale-105 duration-500 ">
                 <img src={t3} alt="" className="w-full rounded-t-md h-80 object-cover" />
               </div>
-              <div className="child bg-black bg-opacity-70 text-white  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
+              <div className="child bg-black bg-opacity-70 text-teal-400  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
                 <h1 className="text-xl font-semibold"> 
                 Harry Porter
                 </h1>
-                <p className="text-sm mt-1 text-gray-500 group-hover:text-white hidden group-hover:block duration-500 ">
+                <p className="text-sm mt-1 text-teal-400 group-hover:text-teal-400 hidden group-hover:block duration-500 ">
                 Flexibility and Yoga Instructor
                 </p>
               </div>
@@ -90,11 +90,11 @@ const Team = () => {
               <div className="parent shadow-md  relative group-hover:transform group-hover:scale-105 duration-500 ">
                 <img src={t2} alt="" className="w-full rounded-t-md h-80 object-cover" />
               </div>
-              <div className="child bg-black bg-opacity-70 text-white  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
+              <div className="child bg-black bg-opacity-70 text-teal-400  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
                 <h1 className="text-xl font-semibold"> 
                 Jessica Brown
                 </h1>
-                <p className="text-sm mt-1 text-gray-500 group-hover:text-white hidden group-hover:block duration-500 ">
+                <p className="text-sm mt-1 text-teal-400 group-hover:text-teal-400 hidden group-hover:block duration-500 ">
                 Weightlifting Coach
                 </p>
               </div>
@@ -105,11 +105,11 @@ const Team = () => {
               <div className="parent shadow-md  relative group-hover:transform group-hover:scale-105 duration-500 ">
                 <img src={t5} alt="" className="w-full rounded-t-md h-80 object-cover" />
               </div>
-              <div className="child bg-black bg-opacity-70 text-white  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
+              <div className="child bg-black bg-opacity-70 text-teal-400  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
                 <h1 className="text-xl font-semibold"> 
                 Alex Rodriguez
                 </h1>
-                <p className="text-sm mt-1 text-gray-500 group-hover:text-white hidden group-hover:block duration-500 ">
+                <p className="text-sm mt-1 text-teal-400 group-hover:text-teal-400 hidden group-hover:block duration-500 ">
                 Strength and Conditioning Specialist
                 </p>
               </div>
@@ -120,11 +120,11 @@ const Team = () => {
               <div className="parent shadow-md  relative group-hover:transform group-hover:scale-105 duration-500 ">
                 <img src={t6} alt="" className="w-full rounded-t-md h-80 object-cover" />
               </div>
-              <div className="child bg-black bg-opacity-70 text-white  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
+              <div className="child bg-black bg-opacity-70 text-teal-400  py-2 px-4 w-full  mx-auto absolute bottom-2  rounded-t-md text-center ">
                 <h1 className="text-xl font-semibold"> 
                 John Doe
                 </h1>
-                <p className="text-sm mt-1 text-gray-500 group-hover:text-white hidden group-hover:block duration-500 ">
+                <p className="text-sm mt-1 text-teal-400 group-hover:text-teal-400 hidden group-hover:block duration-500 ">
                 Senior Fitness Instructor
                 </p>
               </div>
